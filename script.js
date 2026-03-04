2dd0b225b0eef0f4836ffaa42df4352b185f8e4c
Dorline Adelaar <dorlineadelaar@MacBook-Air-de-Dorline.local>
2026-03-04 23:34:46 +0100

    mise à jour du portfolio

---
+++ script.js
@@ -0,0 +1,44 @@
+// On récupère les éléments
+const profilePic = document.getElementById("profile-pic");
+const overlay = document.getElementById("image-overlay");
+
+// Quand on clique sur la photo
+profilePic.addEventListener("click", () => {
+
+  overlay.style.display = "flex"; // affiche l'overlay
+
+  // petit délai pour lancer l'animation
+  setTimeout(() => {
+    overlay.classList.add("active");
+  }, 10);
+
+});
+
+// Quand on clique sur l'arrière-plan
+overlay.addEventListener("click", () => {
+
+  overlay.classList.remove("active");
+
+  // on laisse le temps à l'animation de se fermer
+  setTimeout(() => {
+    overlay.style.display = "none";
+  }, 300);
+
+});
+
+// animation apparition section about
+const aboutSection = document.querySelector(".about-container");
+
+window.addEventListener("scroll", () => {
+
+  const position = aboutSection.getBoundingClientRect().top;
+
+  const screenHeight = window.innerHeight;
+
+  if (position < screenHeight - 100) {
+
+    aboutSection.classList.add("visible");
+
+  }
+
+});
